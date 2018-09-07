7eae7aeb9dcc20a6c7398cdc1dc16dc5092dbfdb
lsde <lsde@users.noreply.github.com>
2018-09-07 18:59:55 +0200

    Update peertube-uploader.py

--- README.md
+++ README.md
@@ -3,15 +3,13 @@
 ## Requirements
  * Python 3
  * python-requests
- * PyYAML
 
 ## Usage
-First, add correct client ID and secret to config.yml.
 ````
 $ pip install -r requirements.txt
 $ ./peertube-uploader.py 
-usage: peertube-uploader.py [-h] -u USERNAME -p PASSWORD -f FILE [-n NAME]
-                            [-c CHANNEL] [-H HOST] [-P PORT] [--config CONFIG]
-                            [--private]
-peertube-uploader.py: error: the following arguments are required: -u/--username, -p/--password, -f/--file
+usage: peertube-uploader.py [-h] [-u USERNAME] [-p PASSWORD] [-c CLIENT_ID]
+                            [-s CLIENT_SECRET] [-t ACCESS_TOKEN] -f FILE
+                            [-n NAME] [-ch CHANNEL] [-e ENDPOINT] [--private]
+peertube-uploader.py: error: argument -f/--file is required
 ````
--- peertube-uploader.py
+++ peertube-uploader.py
@@ -4,27 +4,26 @@ import os
 import sys
 import requests
 import argparse
-import yaml
 from mimetypes import guess_type
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-u', '--username', help='Username', required=True)
-parser.add_argument('-p', '--password', help='Password', required=True)
+parser.add_argument('-u', '--username', help='Username', default=os.getenv('PEERTUBE_USERNAME', 'user'))
+parser.add_argument('-p', '--password', help='Password', default=os.getenv('PEERTUBE_PASSWORD', 'password'))
+parser.add_argument('-c', '--client_id', help='Client ID to use', default=os.getenv('PEERTUBE_CLIENT_ID', 'client_id'))
+parser.add_argument('-s', '--client_secret', help='Client secret to use', default=os.getenv('PEERTUBE_CLIENT_SECRET', 'client_secret'))
+parser.add_argument('-t', '--access_token', help='Access token to use', default=os.getenv('PEERTUBE_ACCESS_TOKEN', 'access_token'))
 parser.add_argument('-f', '--file', help='File to upload', required=True)
 parser.add_argument('-n', '--name', help='Name of the uploaded video')
-parser.add_argument('-c', '--channel', help='Channel ID to upload to', default=1)
-parser.add_argument('-H', '--host', help='Host name', default='localhost')
-parser.add_argument('-P', '--port', help='Port', default=9000)
-parser.add_argument('--config', help='Config file', default='config.yml')
+parser.add_argument('-ch', '--channel', help='Channel ID to upload to', default=os.getenv('PEERTUBE_CHANNEL_ID', '1'))
+parser.add_argument('-e', '--endpoint', help='Host name', default=os.getenv('PEERTUBE_ENDPOINT', 'http://localhost:9000'))
 parser.add_argument('--private', help='Set video as private', action='store_true')
 args = parser.parse_args()
 
-cfg = yaml.load(open(args.config, 'r'))
 file_name = os.path.basename(args.file)
 file_mime_type = guess_type(args.file)[0]
 
-auth_data = {'client_id': cfg['client_id'],
-             'client_secret': cfg['client_secret'],
+auth_data = {'client_id': args.client_id,
+             'client_secret': args.client_secret,
              'grant_type': 'password',
              'response_type': 'code',
              'username': args.username,
@@ -36,26 +35,26 @@ upload_data = {'channelId': args.channel,
                'name': args.name if args.name else file_name
                }
 
-if 'access_token' not in cfg.keys():
+if args.access_token is '':
     try:
-        auth_result = requests.post('http://{0}:{1}{2}'.format(args.host, args.port, cfg['auth_url']), data=auth_data)
+        auth_result = requests.post('{0}{1}'.format(args.endpoint, '/api/v1/users/token'), data=auth_data)
         access_token = (auth_result.json()['access_token'])
     except:
         print(auth_result.text)
         sys.exit(1)
 else:
-    access_token =  cfg['access_token']
+    access_token = args.access_token 
 
 upload_headers = {'Authorization': 'Bearer {0}'.format(access_token)}
 
 with open(args.file, 'rb') as f:
-    upload_result = requests.post('http://{0}:{1}{2}'.format(args.host, args.port, cfg['upload_url']),
+    upload_result = requests.post('{0}{1}'.format(args.endpoint, '/api/v1/videos/upload'),
                                   headers=upload_headers,
                                   data=upload_data,
                                   files={"videofile": (file_name, f, file_mime_type)})
     try:
         video_uuid = upload_result.json()['video']['uuid']
-        print('http://{0}:{1}{2}/{3}'.format(args.host, args.port, cfg['watch_url'], video_uuid))
+        print('{0}{1}/{2}'.format(args.endpoint, '/videos/watch', video_uuid))
     except:
         print(upload_result.text)
         sys.exit(1)
